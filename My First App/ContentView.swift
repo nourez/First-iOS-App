0d125cd4678865a9c1ec11aef4f4b2a6adb4a397
nourez <nourez@nourez.dev>
2021-04-04 19:42:58 -0400

    Initial Commit

---
+++ My First App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  My First App
+//
+//  Created by Nourez Rawji on 2021-04-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
